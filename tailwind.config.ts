5f3d2e0cbd1d78408fe4a033e9af6ec7582292dc
santinakc <santinakcfin@gmail.com>
2023-11-21 01:32:52 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brown: {
+          100: "#D1AC6D",
+          200: "#A56D51",
+          300: "#403736",
+        },
+        skin: {
+          100: "#EAD3C4",
+          200: "#D9B69F",
+        },
+        gray: {
+          100: "#555555",
+          200: "#222222",
+        },
+        background: "#EEF0E1",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
